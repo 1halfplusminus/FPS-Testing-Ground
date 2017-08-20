4abcaf3311e58b6096c8b507de952ab2304b1946
dartuchiwa <sautron.gael@laposte.net>
2017-08-20 17:21:04 +1100

    Change folder structure

--- Source/TestingGround/TP_ThirdPerson/TP_ThirdPersonGameMode.cpp
+++ Source/TestingGround/TP_ThirdPerson/TP_ThirdPersonGameMode.cpp
@@ -6,10 +6,4 @@
 
 ATP_ThirdPersonGameMode::ATP_ThirdPersonGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
 }
