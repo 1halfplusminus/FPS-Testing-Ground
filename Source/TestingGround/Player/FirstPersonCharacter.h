d093720453ab96c423b6c5624d6a177188d045ee
dartuchiwa <sautron.gael@laposte.net>
2017-08-23 22:33:41 +1100

    Owner invisble Meshes

--- Source/TestingGround/Player/FirstPersonCharacter.h
+++ Source/TestingGround/Player/FirstPersonCharacter.h
@@ -14,7 +14,7 @@ class AFirstPersonCharacter : public ACharacter
 	GENERATED_BODY()
 
 	/** Pawn mesh: 1st person view (arms; seen only by self) */
-	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Mesh", meta = (AllowPrivateAccess = "true"))
 	class USkeletalMeshComponent* Mesh1P;
 
 	/** First person camera */
